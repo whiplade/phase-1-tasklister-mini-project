9930ff3e4faf5880052d7036ceb119714fd06b5a
whiplade <nmawira13@gmail.com>
2023-06-24 13:51:07 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+ 
+  form = document.getElementById("create-task-form")
+  form.addEventListener("submit", addToList)
+
+  function addToList(e) {
+
+    e.preventDefault()
+
+    let ul = document.getElementById("tasks")
+    let li = document.createElement("li")
+    let deleteButton = document.createElement("button")
+    deleteButton.innerHTML = "delete"
+    deleteButton.onclick = () => li.remove()
+    li.innerHTML = e.target["new-task-description"].value
+
+    li.style.display = "";
+    li.style.alignItems = "center";
+    li.style.marginBottom = "10px";
+
+    // Add styles to the delete button
+    deleteButton.style.marginLeft = "10px";
+    deleteButton.style.padding = "5px 10px";
+    deleteButton.style.border = "none";
+    deleteButton.style.background = "red";
+    deleteButton.style.color = "white";
+
+    li.appendChild(deleteButton)
+    ul.appendChild(li)
+
+  }
+
+})
+
+
+
+
+
